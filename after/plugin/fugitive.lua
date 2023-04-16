cf40e29193e303a2d3690fba6cdd1615c2d89dff
Sidharth Padhee <sidharthpadhee@gmail.com>
2023-04-16 00:42:12 -0700

    some new remaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,9 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gc", vim.cmd.GlLog)
+vim.keymap.set("n", "<leader>gl", vim.cmd.GlLog)
+vim.keymap.set("n", "<leader>gb", function()
+    vim.cmd('Git blame');
+end)
+vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit)
 vim.keymap.set("n", "g/", vim.cmd.Gvdiffsplit)
 vim.keymap.set("v", "<leader>=", ":'<,'>diffput<CR>")
 vim.keymap.set("v", "<leader>-", ":'<,'>diffget<CR>")
