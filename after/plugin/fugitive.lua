33783f1b9a32035fe6ec44d191029aa14449899f
Sidharth Padhee <sidharthpadhee@gmail.com>
2023-04-16 00:45:38 -0700

    add git pull and push remaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -3,6 +3,12 @@ vim.keymap.set("n", "<leader>gl", vim.cmd.GlLog)
 vim.keymap.set("n", "<leader>gb", function()
     vim.cmd('Git blame');
 end)
+vim.keymap.set("n", "<leader>gp", function()
+    vim.cmd('Git pull --rebase');
+end)
+vim.keymap.set("n", "<leader>gP", function()
+    vim.cmd('Git push -u origin --rebase');
+end)
 vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit)
 vim.keymap.set("n", "g/", vim.cmd.Gvdiffsplit)
 vim.keymap.set("v", "<leader>=", ":'<,'>diffput<CR>")
