9855372c5505fe9d637b5b121094ebc73525caad
Sidharth Padhee <sidharthpadhee@gmail.com>
2023-04-16 00:57:42 -0700

    Add git checkout to fugitive.lua

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -9,6 +9,7 @@ end)
 vim.keymap.set("n", "<leader>gP", function()
     vim.cmd('Git push -u origin');
 end)
+vim.keymap.set("n", "<leader>gc", ':Git checkout ')
 vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit)
 vim.keymap.set("n", "g/", vim.cmd.Gvdiffsplit)
 vim.keymap.set("v", "<leader>=", ":'<,'>diffput<CR>")
