537a19894605102491828698ba4491d341c5ca85
Sidharth Padhee <sidharthpadhee@gmail.com>
2023-05-07 15:47:08 -0700

    Add lsp restart keybinding

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -67,6 +67,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "<leader>va", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>vl", function() vim.cmd.LspRestart() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
