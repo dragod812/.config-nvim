6881b7f0c980f625e2729c8b0337a302e45ba5da
Sidharth Padhee <sidharthpadhee@gmail.com>
2023-06-01 14:23:42 -0700

    harpoon add more remaps, flutter debugging support

--- lua/paddy/packer.lua
+++ lua/paddy/packer.lua
@@ -70,6 +70,14 @@ return require('packer').startup(function(use)
             'nvim-lua/plenary.nvim',
             'stevearc/dressing.nvim', -- optional for vim.ui.select
         },
+        config = function()
+            require('flutter-tools').setup {
+                debugger = {
+                    enabled = true,
+                    run_via_dap = true
+                },
+            }
+        end
     }
 
 
