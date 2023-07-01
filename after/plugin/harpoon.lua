51507978858d6759d32af490c28241dee9d07e26
Sidharth Padhee <sidharthpadhee@gmail.com>
2023-07-01 03:43:54 -0700

    remove overriding the harpoon remaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,9 +8,5 @@ vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end, { noremap = true, si
 vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end, { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end, { noremap = true, silent = true })
 vim.keymap.set("n", "<C-m>", function() ui.nav_file(4) end, { noremap = true, silent = true })
-vim.keymap.set("n", "<C-J>", function() ui.nav_file(5) end, { noremap = true, silent = true })
-vim.keymap.set("n", "<C-K>", function() ui.nav_file(6) end, { noremap = true, silent = true })
-vim.keymap.set("n", "<C-L>", function() ui.nav_file(7) end, { noremap = true, silent = true })
-vim.keymap.set("n", "<C-M>", function() ui.nav_file(8) end, { noremap = true, silent = true })
 
 
