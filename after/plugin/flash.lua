8e62d682fbb176e402a9c9e524a1417ad3b2f959
Sidharth Padhee <sidharthpadhee@gmail.com>
2023-07-01 15:10:02 -0700

    flash remove S

--- after/plugin/flash.lua
+++ after/plugin/flash.lua
@@ -1,22 +1,27 @@
-vim.keymap.set( { "n", "x", "o" }, "s",
-  function()
-    require("flash").jump()
-  end
+vim.keymap.set({ "n", "x", "o" }, "s",
+    function()
+        require("flash").jump({
+            remote_op = {
+                restore = true,
+                motion = nil,
+            },
+        })
+    end
 )
-vim.keymap.set( { "n", "o", "x"}, "S",
-  function()
-    require("flash").treesitter()
-  end
+vim.keymap.set({ "n", "o", "x" }, "S",
+    function()
+        require("flash").treesitter()
+    end
 )
-vim.keymap.set( { "o" }, "r",
-  function()
-   require("flash").remote()
-  end
+vim.keymap.set({ "o" }, "r",
+    function()
+        require("flash").remote()
+    end
 )
-vim.keymap.set( { "o", "x" }, "R",
-  function()
-   require("flash").treesitter_search()
-  end
+vim.keymap.set({ "o", "x" }, "R",
+    function()
+        require("flash").treesitter_search()
+    end
 )
 -- vim.keymap.set( { "c" }, "<c-s>",
 --   function()
